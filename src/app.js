127670e22fa5b6d2573a8b805791a1080e7c1c4e
Felipe Duarte <felipeduarte27@gmail.com>
2020-12-02 21:23:01 -0300

    primeiro commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import routes from './routes';
+// import './database';
+
+// Instanciei o servidor utilizando o express e disse que vou utilizar as rotas e os middlewares
+class App {
+  constructor() {
+    // instanciou o servidor usando o express
+    this.server = express();
+    // chamada de métodos
+    this.middlewares();
+    this.routes();
+  }
+
+  // digo que o servidor vai utilizar middlewares no formato json
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  // digo que o servidor vai utilizar as rotas definidas no arquivo de rotas
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+// exporto a aplicação para ser utilizada em outro arquivo
+export default new App().server;
---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+// instancio as rotas utilizando a classe Router do express
+const routes = new Router();
+
+// routes.post('/storeUser', UserController.store);
+// routes.post('/login', SessionController.store);
+
+// exorto as rotas
+export default routes;
