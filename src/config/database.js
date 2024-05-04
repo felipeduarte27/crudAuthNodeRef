02c47dfcd82809a13deb7528217f1ee41b3c4b39
Felipe Lima Duarte <felipeduarte27@gmail.com>
2024-05-04 18:26:10 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: '123',
+  password: '',
   database: 'crudnoderef',
   define: {
     timestamps: true,
